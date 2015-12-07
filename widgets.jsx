f46b731fc80ebee7964d6589d1c492355d746c32
App Academy Student <student@appacademy.io>
2015-12-07 10:13:33 -0800

    initial commit

---
+++ widgets.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(<MyComponent />, document.getElementById('main'));
+});
